366d5444a04b486b7d39004ad09c9df43096a895
ablaev.dmitry <dablaev@yandex.ru>
2020-10-07 17:51:15 +0700

    Рефакторинг

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-const PORT = process.env.port || 80;
+const PORT = process.env.PORT || 80;
 const DIST_DIR = path.join(__dirname, 'dist');
 const INDEX_FILE_PATH = path.join(DIST_DIR, 'index.html');
 
